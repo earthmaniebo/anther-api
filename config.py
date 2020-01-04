4d07ced672a00537599e28e3e9bedb2a95e7e74f
Earth Maniebo <earthmaniebo@gmail.com>
2020-01-05 02:53:37 +0800

    Initial commit

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+
+# Configure database URI.
+# Format is 'mysql://<username>:<password>@<hostname>/<database_name>'
+SQLALCHEMY_DATABASE_URI = "mysql://anther:anther12345@127.0.0.1/anther"
